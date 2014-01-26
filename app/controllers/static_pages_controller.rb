90c6bdaea84621772a79c4ef8144b0deda1a8cc4
Carla Cruz <carla13cruz@gmail.com>
2014-01-26 12:25:49 -0800

    initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+class StaticPagesController < ApplicationController
+
+def index
+end
+
+def about
+end
+
+def products
+end
+
+def services
+end
+
+def policies
+end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BellaDonna::Application.config.secret_key_base = '2f5fabd79dc7b630c73682c2dbadaadda97ba66c7efd361d212ed6d42d2f8581e9d39db5f0121ace830e8dda63b574978ce628d743e6625ca9da31c3194e13fd'
