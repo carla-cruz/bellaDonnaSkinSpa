0c6003fc211ea36b0ee724c6eeda89f42f6add35
Paulette Luftig <pyluftig@gmail.com>
2014-02-23 09:44:33 -0800

    added index for appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,7 @@
 class AppointmentsController < ApplicationController
-
+	before_filter :is_admin?, except: [:index]
 	def index
+		@appointment = Appointment.all
 	end
 
 	def new
@@ -15,25 +16,30 @@ class AppointmentsController < ApplicationController
 		@appointment = Appointment.create!(appointment_params)
 		if @appointment.save
 			flash[:alert] = "Your request has been sent!"
-		redirect_to root_path
+			redirect_to root_path
 		else
 			flash[:alert] = "Uh oh!"
 			redirect(:back)
 		end
 	end
 
-		def edit
-		end
+	def edit
+	end
 
-		def update
-		end
+	def update
+	end
 
-		def destroy
+	def destroy
+		@appointment = Appointment.destroy(params[:id])
+		respond_to do |format|
+			format.html { redirect_to appointments_path }
+			format.js
 		end
+	end
 
-private
+	private
 	def appointment_params
 		params.require(:appointment).permit(:first_name, :last_name, :phone_number, :is_new, :first_date, :first_time, :second_date, :second_time, :third_date, :third_time, :appointment_type, :comments)
 	end
 
-	end
+end
