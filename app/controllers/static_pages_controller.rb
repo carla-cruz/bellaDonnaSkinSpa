1430634c8515b65aa3fd1412350f137629a1d395
Paulette Luftig <pyluftig@gmail.com>
2014-03-16 10:01:25 -0700

    remove about page and code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,4 @@ class StaticPagesController < ApplicationController
   def index
     @promotion = Promotion.where('is_active = ?', true)[0]
   end
-
-  def about
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ BellaDonna::Application.routes.draw do
 
   devise_for :users
   root :to => "static_pages#index"
-  get '/about' => 'static_pages#about', :as => :about
   get '/contact' => 'static_pages#contact', :as => :contact
 
   resources :products do 
