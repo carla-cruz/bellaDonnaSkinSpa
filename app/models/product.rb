78c53fdc78a10cd5fdf762c3825053be8d87b6d4
Paulette Luftig <pyluftig@gmail.com>
2014-04-26 13:38:23 -0700

    remove presence of validation for description of product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories
   accepts_nested_attributes_for :categories
 
-  validates :name, :description, :price, presence: true
+  validates :name, :price, presence: true
   validates :name, uniqueness: true
 end
