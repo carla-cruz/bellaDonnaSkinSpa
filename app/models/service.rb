1d24dfde8f01e6f19e06e2248a4b77048aa5b53c
Paulette Luftig <pyluftig@gmail.com>
2014-04-26 13:43:24 -0700

    correction - meant to remove description present validation from service

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,6 @@ class Service < ActiveRecord::Base
 
   belongs_to :service_type
 
-  validates :title, :price, :description, presence: true
+  validates :title, :price, presence: true
   validates :title, uniqueness: true
 end
